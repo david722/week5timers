63773375e5157002bf22f5fc7432997b86637b39
David Olazabal <davidolazabal@UM-W0C5HV2R.local>
2018-04-23 20:48:32 -0400

    lots of changes

---
+++ assets/javascript/app.js
@@ -0,0 +1,235 @@
+$(document).ready(function() {
+
+	var currentQuestion;	
+
+	var correctAnswer;	
+	var wrongAnswer;	
+	var unanswered;		
+
+	var answered; 		
+	
+						
+	var seconds;		
+	var time;			
+	
+	var userChoice;		
+
+	var text = {
+		correct: "Correct",
+		incorrect: "Try again!",
+		noTime: "Aaaaand you failed!",
+		done: "Finally done!",
+	};
+
+	var triviaQuestions = [
+		{	
+			question: "Who was the legendary Benedictine monk who invented champagne?",
+			choices: ["Francis", "Jose Cuervo", "Dom Perignon", "Sam Admas"],
+			correct: 2,
+			image: "assets/images/domperignon.jpg",
+			answerText: "Dom Perignon",
+		},
+
+		{
+			question: "Name the largest freshwater lake in the world?",
+			choices: ["Lake Minotonca", "Lake Superior", "Lake More Superior", "Big Daddy Lake"],
+			correct: 1,
+			image: "assets/images/lakesuperior.jpg",
+			answerText: "Lake Superior",
+		},
+
+		{
+			question: "What is the diameter of Earth?",
+			choices: ["1,000,000 miles", "80,000 miles", "9,000 miles", "8,000 miles"],
+			correct: 3,
+			image: "assets/images/8000.png",
+			answerText: "The Earth's diameter is 8,000 miles",
+		},
+
+		{
+			question: "Which chess piece can only move diagonally?",
+			choices: ["Queen", "King", "Daddy", "Bishop"],
+			correct: 3,
+			image: "assets/images/bishop.jpg",
+			answerText: "Bishop",
+		},
+		{
+			question: "How many valves does a trumpet have?",
+			choices: ["5", "3", "4", "1"],
+			correct: 1,
+			image: "assets/images/3.jpeg",
+			answerText: '3!',
+		},
+		{
+			question: "When did the Cold War end?",
+			choices: ["1989", "2001", "2018", "1942"],
+			correct: 0,
+			image: "assets/images/coldwar.jpg",
+			answerText: 'The Cold War ended in 1989.'
+		},
+		{
+			question: "What is the oldest surviving printed book in the world?",
+			choices: ["Kama Sutra", "Epic of Gilgamesh", "The Bible", "The Diamond Sutra"],
+			correct: 3,
+			image: "assets/images/diamondsutra.jpg",
+			answerText: "The oldest printed book is the Diamond Sutra"
+		},
+	];
+
+	// Hides Content at Start Up
+	$("#gameArea").hide();
+
+	// Start Button Click and Hide
+	$("#startBtn").on("click", function(){
+		$("#startGame").hide();
+		newGame();
+	});
+
+	// Reset Button
+	$("#startOverBtn").on("click", function(){
+		$("#Res").hide();
+		newGame();
+	});
+
+	// ======================================================
+	//Function to Start Game After Initial Click
+	// ======================================================
+	function newGame() {
+		$("#gameArea").show();
+		$("#Ans").hide();
+		$("#Res").hide();		
+		correctAnswer = 0;
+		wrongAnswer = 0;
+		unanswered = 0;
+		currentQuestion = 0;
+		questions();
+	}
+	// ==================
+	// Displays Question
+	// ==================
+	function questions() {
+		$("#Ans").hide();
+		$("#Qs").show();
+		answered = true;
+		// Prints Question from Array
+		$(".question").html(triviaQuestions[currentQuestion].question);
+
+		// -----------------------------------------
+		//Loops through possible choices and appends
+		// -----------------------------------------
+		for (var i = 0; i <= 4; i++) {
+			var list = $("<div>");
+			list.text(triviaQuestions[currentQuestion].choices[i]);
+			list.attr({"data-index": i });
+			list.addClass("thisChoice");
+			$(".choices").append(list);
+		}
+
+		//Calls Timer
+		countdown();
+
+		// USERCLICK
+		$(".thisChoice").on("click",function(){
+			userChoice = $(this).data("index");
+			clearInterval(time);
+			shoAnswer();
+		});
+	}
+
+	// ==================
+	// TIMER COUNTDOWN
+	// ==================
+	function countdown() {
+		seconds = 20;
+		$("#time").html("00:" + seconds);
+		answered = true;
+		//Delay of 1 sec before timer goes off
+		time = setInterval(countDownSho, 1000);
+	}
+
+	// ==================
+	// SHOWS TIMER
+	// ==================
+	function countDownSho() {
+		seconds --;
+		if(seconds < 10) {
+			$("#time").html("00:0" + seconds);
+			$("#time").css({"color": "red"});
+		} else {
+			$("#time").html("00:" + seconds);
+			$("#time").css({"color": "#def"});
+		}
+
+		if (seconds < 1) {
+			clearInterval(time);
+			answered = false;
+			shoAnswer();
+		}
+	}
+	// ====================================
+	// DISPLAYS ANSWER DIV
+	// ====================================
+	function shoAnswer() {
+		$("#Qs").hide();
+		$("#Res").hide();
+		$("#Ans").show();
+		$(".thisChoice").empty();
+
+		var rightAnswerText = triviaQuestions[currentQuestion].choices[triviaQuestions[currentQuestion].correct];
+		var rightAnswerIndex = triviaQuestions[currentQuestion].correct;
+		console.log(rightAnswerText);
+		console.log(rightAnswerIndex);
+		//GIF IMG
+		var gifLink = triviaQuestions[currentQuestion].image;
+		var Giffy = $("<img>");
+		Giffy.attr("Src", gifLink);
+		Giffy.addClass("gifImg");
+		$("#gif").html(Giffy);
+		// GIF TEXT
+		var gifText = triviaQuestions[currentQuestion].answerText;
+			newCap = $("<div>");
+			newCap.html(gifText);
+			newCap.addClass("gifCap");
+			$("#gifText").html(newCap);
+
+
+		// DISPLAYS AND COUNTS USER ANSWERS/ UnANSWERS
+		if ((userChoice === rightAnswerIndex) && (answered === true)) {
+			correctAnswer++;
+			$("#text").html(text.correct);
+			$("#correctAnswer").hide();
+		} else if ((userChoice !== rightAnswerIndex) && (answered === true)) {
+			wrongAnswer++;
+			$("#text").html(text.incorrect);
+			$("#correctAnswer").show().html("The correct answer was: " + rightAnswerText);
+		} else {
+			unanswered++;
+			$("#text").html(text.noTime);
+			$("#correctAnswer").html("The correct answer was: " + rightAnswerText);
+			answered = true;
+		}
+
+		//Last Answer Reveal Timer
+		if (currentQuestion === (triviaQuestions.length-1)) {
+			setTimeout(results, 10000);
+		} else {
+			currentQuestion++;
+			setTimeout(questions, 10000);
+		}
+
+	}
+
+	function results() {
+		$("#Ans").hide();
+		$("#Qs").hide();
+		$("#Res").show();
+		$("#resultText").html(text.done);
+		$("#correctAnswers").html("Correct Answers: " + correctAnswer);
+		$("#wrongAnswers").html("Wrong Answers: " + wrongAnswer);
+		$("#unanswered").html("Didn't Answer: " + unanswered);
+		$("#startOverBtn").show();
+		$("#startOverBtn").html("RESTART GAME");
+	}
+
+	
+});
